f8822732add2b6022246aacb429a1d0dd7ae6b4a
Cristian <crisszz7@outlook.com>
2025-12-16 18:16:06 -0500

    Commit: Style's table changed

--- frontend/src/pages/Admin/components/IU/TableComponent.jsx
+++ frontend/src/pages/Admin/components/IU/TableComponent.jsx
@@ -3,15 +3,22 @@ export const TableComponent = ({title}) => {
     return(
         <div className="w-full h-max flex flex-col items-center mt-5 p-3 text-white">
             <h2 className="font-bold w-full text-left text-2xl"> {title} </h2>
+
             <table className="border border-white lg:h-92 w-11/12 mt-3 rounded-2xl">
-                <thead className="rounded-md ">
-                    <tr className=" rounded-xs border border-white ">
-                        <th className="py-5 bg-neutral-800">Header 1</th>
+                <thead>
+                    <tr className=" rounded-xs border border-white">
+                        <th colspan="6" className="py-5 bg-neutral-800">Header 1</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    <tr >
                         <td className="p-2 bg-neutral-900 opacity-90"> Info 1</td>
+                        <td className="p-2 bg-neutral-900 opacity-90"> Info 1</td>
+                        <td className="p-2 bg-neutral-900 opacity-90"> Info 1</td>
+                        <td className="p-2 bg-neutral-900 opacity-90"> Info 1</td>
+                        <td className="p-2 bg-neutral-900 opacity-90"> Info 1</td>
+                        <td className="p-2 bg-neutral-900 opacity-90"> Info 1</td>
+
                     </tr>
                 </tbody>
             </table>
